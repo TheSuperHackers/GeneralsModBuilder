2497f92ac143f314116f7ead0deaafa37807e09f
xezon <4720891+xezon@users.noreply.github.com>
2022-04-02 16:40:02 +0200

    Add single build dir folder to ModFolders.json for simplicity

--- ModBuilder/Scripts/Python/data/folders.py
+++ ModBuilder/Scripts/Python/data/folders.py
@@ -7,9 +7,7 @@ from dataclasses import dataclass
 class Folders:
     releaseUnpackedDir: str
     releaseDir: str
-    tmpBigUnpackedDir: str
-    tmpBigDir: str
-    tmpReleaseUnpackedDir: str
+    buildDir: str
 
     def __init__(self):
         pass
@@ -17,25 +15,19 @@ class Folders:
     def Normalize(self) -> None:
         self.releaseUnpackedDir = utils.NormalizePath(self.releaseUnpackedDir)
         self.releaseDir = utils.NormalizePath(self.releaseDir)
-        self.tmpBigUnpackedDir = utils.NormalizePath(self.tmpBigUnpackedDir)
-        self.tmpBigDir = utils.NormalizePath(self.tmpBigDir)
-        self.tmpReleaseUnpackedDir = utils.NormalizePath(self.tmpReleaseUnpackedDir)
+        self.buildDir = utils.NormalizePath(self.buildDir)
 
     def VerifyTypes(self) -> None:
         utils.RelAssert(isinstance(self.releaseUnpackedDir, str), "Folders.releaseUnpackedDir has incorrect type")
         utils.RelAssert(isinstance(self.releaseDir, str), "Folders.releaseDir has incorrect type")
-        utils.RelAssert(isinstance(self.tmpBigUnpackedDir, str), "Folders.tmpBigUnpackedDir has incorrect type")
-        utils.RelAssert(isinstance(self.tmpBigDir, str), "Folders.tmpBigDir has incorrect type")
-        utils.RelAssert(isinstance(self.tmpReleaseUnpackedDir, str), "Folders.tmpReleaseUnpackedDir has incorrect type")
+        utils.RelAssert(isinstance(self.buildDir, str), "Folders.buildDir has incorrect type")
 
 
 def MakeFoldersFromJsons(jsonFiles: list[JsonFile]) -> Folders:
     folders = Folders()
     folders.releaseUnpackedDir = None
     folders.releaseDir = None
-    folders.tmpBigUnpackedDir = None
-    folders.tmpBigDir = None
-    folders.tmpReleaseUnpackedDir = None
+    folders.buildDir = None
 
     for jsonFile in jsonFiles:
         jsonDir: str = utils.GetFileDir(jsonFile.path)
@@ -44,9 +36,7 @@ def MakeFoldersFromJsons(jsonFiles: list[JsonFile]) -> Folders:
         if jFolders:
             folders.releaseUnpackedDir = utils.JoinPathIfValid(folders.releaseUnpackedDir, jsonDir, jFolders.get("releaseUnpackedDir"))
             folders.releaseDir = utils.JoinPathIfValid(folders.releaseDir, jsonDir, jFolders.get("releaseDir"))
-            folders.tmpBigUnpackedDir = utils.JoinPathIfValid(folders.tmpBigUnpackedDir, jsonDir, jFolders.get("tmpBigUnpackedDir"))
-            folders.tmpBigDir = utils.JoinPathIfValid(folders.tmpBigDir, jsonDir, jFolders.get("tmpBigDir"))
-            folders.tmpReleaseUnpackedDir = utils.JoinPathIfValid(folders.tmpReleaseUnpackedDir, jsonDir, jFolders.get("tmpReleaseUnpackedDir"))
+            folders.buildDir = utils.JoinPathIfValid(folders.buildDir, jsonDir, jFolders.get("buildDir"))
 
     folders.VerifyTypes()
     folders.Normalize()
