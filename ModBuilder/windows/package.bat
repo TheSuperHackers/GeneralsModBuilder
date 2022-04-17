8d4a6473d1314f9c897abf0eebd094a5cef6174f
xezon <4720891+xezon@users.noreply.github.com>
2022-04-17 18:58:42 +0200

    Change tool version

--- ModBuilder/windows/package.bat
+++ ModBuilder/windows/package.bat
@@ -5,7 +5,7 @@ set ThisDir=%~dp0.
 call "%ThisDir%\setupfolders.bat"
 
 %PythonExe% -m venv "%VenvDir%"
-%VenvExe% -m pip install wheel
+%VenvExe% -m pip install wheel==0.37.1
 %VenvExe% -m pip install -r "%ProjDir%\requirements.txt"
 %VenvExe% -m pip install -r "%ProjDir%\requirements-dev.txt"
 
--- ModBuilder/windows/pyinstaller.bat
+++ ModBuilder/windows/pyinstaller.bat
@@ -5,7 +5,7 @@ set ThisDir=%~dp0.
 call "%ThisDir%\setupfolders.bat"
 
 %PythonExe% -m venv "%VenvDir%"
-%VenvExe% -m pip install pyinstaller
+%VenvExe% -m pip install pyinstaller==4.10
 %VenvExe% -m pip install -r "%ProjDir%\requirements.txt"
 %VenvExe% -m pip install -r "%ProjDir%\requirements-dev.txt"
 
