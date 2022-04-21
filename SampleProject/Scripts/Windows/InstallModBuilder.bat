a2683d072521de6a1c6d26b59951582dd8472d1b
xezon <4720891+xezon@users.noreply.github.com>
2022-04-21 21:14:38 +0200

    Remove generalsmodbuilder.exe hashing because it is not necessary

--- SampleProject/Scripts/Windows/InstallModBuilder.bat
+++ SampleProject/Scripts/Windows/InstallModBuilder.bat
@@ -6,33 +6,40 @@ set WasInstalled=0
 
 call "%ThisDir%\Setup.bat"
 
-echo Hash Generals Mod Builder Exe ...
-Certutil -hashfile "%ModBuilderExe%" SHA256 | findstr /c:%ModBuilderExeSha256%
-
-if %errorlevel% NEQ 0 (
-    echo Installing Generals Mod Builder at '%ModBuilderDir%'
+if not exist %ModBuilderExe% (
+    echo Installing Generals Mod Builder at '%ModBuilderDir%' ...
 
     if not exist "%ModBuilderDir%" (
-        echo Create folder '%ModBuilderDir%'
         mkdir "%ModBuilderDir%"
     )
 
-    echo Download '%ModBuilderZipUrl%'
+    echo Download '%ModBuilderZipUrl%' ...
+
     curl --location "%ModBuilderZipUrl%" --output "%ModBuilderZip%" --max-filesize %ModBuilderZipSize%
 
     set WasDownloaded=1
+) else (
+    echo Generals Mod Builder is installed
 )
 
 if %WasDownloaded% NEQ 0 (
     if exist "%ModBuilderZip%" (
+        echo Check Generals Mod Builder archive ...
+        for /F %%I in ("%ModBuilderZip%") do (
+            if %%~zI NEQ %ModBuilderZipSize% (
+                echo File '%ModBuilderZip%' does not have expected size %ModBuilderZipSize%
+                exit /B 222
+            )
+        )
+
         echo Hash Generals Mod Builder archive ...
         Certutil -hashfile "%ModBuilderZip%" SHA256 | findstr /c:%ModBuilderZipSha256%
 
         if %errorlevel% EQU 0 (
-            echo Extract archive '%ModBuilderZip%'
+            echo Extract archive '%ModBuilderZip%' ...
             tar.exe -x -k -f "%ModBuilderZip%" -C "%ModBuilderDir%" --strip-components=1
 
-            echo Delete archive '%ModBuilderZip%'
+            echo Delete archive '%ModBuilderZip%' ...
             del /q "%ModBuilderZip%"
 
             set WasInstalled=1
@@ -49,22 +56,6 @@ if %WasDownloaded% NEQ 0 (
 if %WasInstalled% NEQ 0 (
     if not exist "%ModBuilderExe%" (
         echo File '%ModBuilderExe%' failed to install
-        exit /B 222
-    )
-
-    echo Hashing Generals Mod Builder ...
-    Certutil -hashfile "%ModBuilderExe%" SHA256 | findstr /c:%ModBuilderExeSha256%
-
-    if %errorlevel% NEQ 0 (
-        echo File '%ModBuilderExe%' does not have expected hash '%ModBuilderExeSha256%'
-        exit /B 222
-    )
-)
-
-echo Check Generals Mod Builder exe ...
-for /F %%I in ("%ModBuilderExe%") do (
-    if %%~zI NEQ %ModBuilderExeSize% (
-        echo File '%ModBuilderExe%' does not have expected size %ModBuilderExeSize%
         exit /B 222
     )
 )
--- SampleProject/Scripts/Windows/Setup.bat
+++ SampleProject/Scripts/Windows/Setup.bat
@@ -5,8 +5,6 @@ set SetupDir=%~dp0.
 set ModBuilderVer=1.1
 set ModBuilderDir=%SetupDir%\.GeneralsModBuilder\v%ModBuilderVer%
 set ModBuilderExe=%ModBuilderDir%\generalsmodbuilder.exe
-set ModBuilderExeSha256=edb06b9c030dd9455b696edbe792cd12e6952cfb7541cd7621162ed7e527a261
-set ModBuilderExeSize=2374726
 set ModBuilderZip=%ModBuilderDir%\generalsmodbuilder.zip
 set ModBuilderZipUrl=https://github.com/TheSuperHackers/GeneralsTools/raw/main/Tools/generalsmodbuilder/v%ModBuilderVer%/generalsmodbuilder_v%ModBuilderVer%.zip
 set ModBuilderZipSha256=e8f30276d0895b34d17d8c5b5cd2e0c7e75ec6c7f4e3fa897ca175464241294e
@@ -24,8 +22,6 @@ if [%~1]==[print] (
     echo ver... %ModBuilderVer%
     echo dir... %ModBuilderDir%
     echo exe... %ModBuilderExe%
-    echo exesha %ModBuilderExeSha256%
-    echo exesiz %ModBuilderExeSize%
     echo zip... %ModBuilderZip%
     echo zipurl %ModBuilderZipUrl%
     echo zipsha %ModBuilderZipSha256%
