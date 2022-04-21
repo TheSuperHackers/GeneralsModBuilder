d29723fc642f74c14a5f1c4e72746af1fe149675
xezon <4720891+xezon@users.noreply.github.com>
2022-04-21 21:23:21 +0200

    Update Mod Builder Hashes in sample scripts

--- SampleProject/Scripts/Windows/Setup.bat
+++ SampleProject/Scripts/Windows/Setup.bat
@@ -7,8 +7,8 @@ set ModBuilderDir=%SetupDir%\.GeneralsModBuilder\v%ModBuilderVer%
 set ModBuilderExe=%ModBuilderDir%\generalsmodbuilder.exe
 set ModBuilderZip=%ModBuilderDir%\generalsmodbuilder.zip
 set ModBuilderZipUrl=https://github.com/TheSuperHackers/GeneralsTools/raw/main/Tools/generalsmodbuilder/v%ModBuilderVer%/generalsmodbuilder_v%ModBuilderVer%.zip
-set ModBuilderZipSha256=e8f30276d0895b34d17d8c5b5cd2e0c7e75ec6c7f4e3fa897ca175464241294e
-set ModBuilderZipSize=7885060
+set ModBuilderZipSha256=8d6338bfb41d7d4761c7c4bbac424bad7875a92a3af82492bf02b1d8ac036acc
+set ModBuilderZipSize=7885084
 
 set ConfigDir=%SetupDir%\..\..
 set ConfigFiles=^
