18b9c29acb56ea4e2f24fecf260733f867a3bffc
xezon <4720891+xezon@users.noreply.github.com>
2022-04-30 12:35:01 +0200

    Beautify Setup.bat

--- SampleProject/Scripts/Windows/Setup.bat
+++ SampleProject/Scripts/Windows/Setup.bat
@@ -2,32 +2,33 @@
 
 set SetupDir=%~dp0.
 
+:: Version, size and hash. Sets which Mod Builder is used.
 set ModBuilderVer=1.4
+set ModBuilderArcSize=30082734
+set ModBuilderArcSha256=97719efd0108ef56a14b42de937576648a8b9295272cc18d47ba50fd56dfb512
+
+:: The mod config files. Relative to this setup file.
+set ConfigFiles=^
+    "%SetupDir%\..\..\ModBundleItems.json" ^
+    "%SetupDir%\..\..\ModBundlePacks.json" ^
+    "%SetupDir%\..\..\ModFolders.json" ^
+    "%SetupDir%\..\..\ModRunner.json"
+
+:: Misc path setup.
 set ModBuilderDir=%SetupDir%\.modbuilder\v%ModBuilderVer%
 set ModBuilderExe=%ModBuilderDir%\generalsmodbuilder\generalsmodbuilder.exe
 set ModBuilderArc=%ModBuilderDir%\generalsmodbuilder.7z
 set ModBuilderArcUrl=https://github.com/TheSuperHackers/GeneralsTools/raw/main/Tools/generalsmodbuilder/v%ModBuilderVer%/generalsmodbuilder_v%ModBuilderVer%.7z
-set ModBuilderArcSha256=97719efd0108ef56a14b42de937576648a8b9295272cc18d47ba50fd56dfb512
-set ModBuilderArcSize=30082734
 
-set ConfigDir=%SetupDir%\..\..
-set ConfigFiles=^
-    "%ConfigDir%\ModBundleItems.json" ^
-    "%ConfigDir%\ModBundlePacks.json" ^
-    "%ConfigDir%\ModFolders.json" ^
-    "%ConfigDir%\ModRunner.json"
-
-if [%~1]==[print] (
-    echo SETUP. Using Generals Mod Builder:
-    echo ver... %ModBuilderVer%
-    echo dir... %ModBuilderDir%
-    echo exe... %ModBuilderExe%
-    echo archiv %ModBuilderArc%
-    echo arcurl %ModBuilderArcUrl%
-    echo arcsha %ModBuilderArcSha256%
-    echo arcsiz %ModBuilderArcSize%
-    for %%f in (%ConfigFiles%) do (
-        echo config %%f
-    )
-    echo SETUP END.
+:: Print setup info.
+echo SETUP.BAT
+echo modver %ModBuilderVer%
+echo arcsiz %ModBuilderArcSize%
+echo arcsha %ModBuilderArcSha256%
+for %%f in (%ConfigFiles%) do (
+    echo config %%f
 )
+echo moddir %ModBuilderDir%
+echo modexe %ModBuilderExe%
+echo arcfil %ModBuilderArc%
+echo arcurl %ModBuilderArcUrl%
