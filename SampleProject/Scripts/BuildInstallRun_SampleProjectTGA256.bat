af56d9f4fb61ed65de20e578b1759417933ece87
xezon <4720891+xezon@users.noreply.github.com>
2022-08-09 16:52:32 +0200

    Add new sample script to install specific bundle pack with

---
+++ SampleProject/Scripts/BuildInstallRun_SampleProjectTGA256.bat
@@ -0,0 +1,28 @@
+@echo off
+
+setlocal
+
+set ThisDir=%~dp0.
+
+call "%ThisDir%\Windows\RequestAdmin.bat" "%~s0" %*
+
+if %errorlevel% EQU 111 (
+    exit /B %errorlevel%
+)
+
+call "%ThisDir%\Windows\InstallModBuilder.bat"
+
+if %errorlevel% EQU 222 (
+    exit /B %errorlevel%
+)
+
+call "%ThisDir%\Windows\Setup.bat"
+
+call "%ModBuilderExe%" ^
+  --build ^
+  --install SampleProjectTGA256 ^
+  --run ^
+  --uninstall ^
+  --config-list %ConfigFiles% %*
+
+endlocal
