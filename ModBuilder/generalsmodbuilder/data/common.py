c2aaadf33f66cb9934acd975435be6764f69b7e2
xezon <4720891+xezon@users.noreply.github.com>
2023-02-26 14:46:33 +0100

    Fix and simplify tools aliases setup

--- ModBuilder/generalsmodbuilder/data/common.py
+++ ModBuilder/generalsmodbuilder/data/common.py
@@ -7,38 +7,6 @@ ParamT = Union[str, int, float, bool]
 ParamsT = dict[str, Union[str, int, float, bool, list[ParamT]]]
 
 
-def __MakeAbsolutePathIfApplicable(path: str, absDir: str) -> str | None:
-    newpath = path.removeprefix("{THIS_DIR}")
-    if newpath != path:
-        newpath = newpath.removeprefix("/")
-        newpath = newpath.removeprefix("\\")
-        newpath = os.path.join(absDir, newpath)
-        newpath = os.path.normpath(newpath)
-        return newpath
-    return None
-
-
-def ProcessParams(params: ParamsT, absDir: str = "") -> ParamsT:
-    newparams = params.copy()
-
-    for key,value in newparams.items():
-        if isinstance(value, str):
-            newValue = __MakeAbsolutePathIfApplicable(value, absDir)
-            if newValue:
-                newparams[key] = newValue
-            continue
-
-        if isinstance(value, list):
-            for index, subValue in enumerate(value):
-                if isinstance(subValue, str):
-                    newValue = __MakeAbsolutePathIfApplicable(subValue, absDir)
-                    if newValue:
-                        newparams[key][index] = newValue
-            continue
-
-    return newparams
-
-
 def VerifyParamsType(params: ParamsT, name: str) -> None:
     for key,value in params.items():
         util.VerifyType(key, str, f"{name}.key")
