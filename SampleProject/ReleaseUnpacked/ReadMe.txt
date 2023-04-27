c9f4c705cdb51779f3be35ea4eb504eecaabe772
xezon <4720891+xezon@users.noreply.github.com>
2023-04-27 18:28:59 +0200

    Remove all sample project files

--- SampleProject/ReleaseUnpacked/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-A majestic ReadMe is always much appreciated when handing out a Mod.
-
-This important message was brought to you by TheSuperHackers.
-
-https://github.com/TheSuperHackers
--- SampleProject/Scripts/Python/OnPreBuildPack.py
+++
@@ -1,8 +0,0 @@
-# This python script is called on build via ModBundlePacks.json configuration
-
-def OnEvent(**kwargs) -> None:
-    print("OnPreBuildPack.py called ...")
-
-    bundlePack = kwargs.get("_bundlePack")
-
-    assert bundlePack != None, "_bundlePack kwargs not found"
