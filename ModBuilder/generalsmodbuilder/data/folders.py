13b9c282284d0d8005a6a730eaa14f9e0ee3dfbd
xezon <4720891+xezon@users.noreply.github.com>
2024-06-20 17:32:59 +0200

    Remove the "ReleaseUnpacked" directory

--- ModBuilder/generalsmodbuilder/data/folders.py
+++ ModBuilder/generalsmodbuilder/data/folders.py
@@ -6,27 +6,22 @@ from generalsmodbuilder import util
 
 @dataclass(init=False)
 class Folders:
-    absReleaseUnpackedDir: str
     absReleaseDir: str
     absBuildDir: str
 
     def __init__(self):
-        self.absReleaseUnpackedDir = None
         self.absReleaseDir = None
         self.absBuildDir = None
 
     def Normalize(self) -> None:
-        self.absReleaseUnpackedDir = os.path.normpath(self.absReleaseUnpackedDir)
         self.absReleaseDir = os.path.normpath(self.absReleaseDir)
         self.absBuildDir = os.path.normpath(self.absBuildDir)
 
     def VerifyTypes(self) -> None:
-        util.VerifyType(self.absReleaseUnpackedDir, str, "Folders.absReleaseUnpackedDir")
         util.VerifyType(self.absReleaseDir, str, "Folders.absReleaseDir")
         util.VerifyType(self.absBuildDir, str, "Folders.absBuildDir")
 
     def VerifyValues(self) -> None:
-        util.Verify(util.IsValidPathName(self.absReleaseUnpackedDir), f"Folders.absReleaseUnpackedDir '{self.absReleaseUnpackedDir}' is not a valid path name")
         util.Verify(util.IsValidPathName(self.absReleaseDir), f"Folders.absReleaseDir '{self.absReleaseDir}' is not a valid path name")
         util.Verify(util.IsValidPathName(self.absBuildDir), f"Folders.absBuildDir '{self.absBuildDir}' is not a valid path name")
 
@@ -39,7 +34,6 @@ def MakeFoldersFromJsons(jsonFiles: list[JsonFile]) -> Folders:
         jFolders: dict = jsonFile.data.get("folders")
 
         if jFolders:
-            folders.absReleaseUnpackedDir = util.JoinPathIfValid(folders.absReleaseUnpackedDir, jsonDir, jFolders.get("releaseUnpackedDir"))
             folders.absReleaseDir = util.JoinPathIfValid(folders.absReleaseDir, jsonDir, jFolders.get("releaseDir"))
             folders.absBuildDir = util.JoinPathIfValid(folders.absBuildDir, jsonDir, jFolders.get("buildDir"))
 
